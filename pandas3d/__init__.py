c5c40267b7ffbe7d724be994ca8c7d497d417839
eycjur <eycjur@gmail.com>
2022-03-20 11:15:22 +0900

    [add]version info

--- pandas3d/__init__.py
+++ pandas3d/__init__.py
@@ -2,5 +2,4 @@
 
 from pandas3d.frame.frame import GridFrame, empty, from_pandas, zeros
 from pandas3d.util.logging import get_logger, root_logger
-
-__version__ = "0.1.0"
+from pandas3d.version import __version__
